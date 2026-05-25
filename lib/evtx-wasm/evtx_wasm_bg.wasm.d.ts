88f2d706159e353276e20d56b30d2ac2b77c4c47
Florian Amette <50982737+florianamette@users.noreply.github.com>
2026-05-24 20:20:46 -0400

    fix(wasm): track wasm-pack output so Vercel builds without a Rust toolchain

---
+++ lib/evtx-wasm/evtx_wasm_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_evtxhandle_free: (a: number, b: number) => void;
+export const evtxhandle_count: (a: number) => bigint;
+export const evtxhandle_event_id_counts: (a: number) => [number, number, number];
+export const evtxhandle_get_chunk: (a: number, b: bigint, c: bigint) => [number, number, number];
+export const evtxhandle_get_event_data: (a: number, b: bigint) => [number, number, number];
+export const evtxhandle_get_event_data_batch: (a: number, b: number, c: number) => [number, number, number];
+export const evtxhandle_get_xml: (a: number, b: bigint) => [number, number, number, number];
+export const evtxhandle_new: (a: number, b: number) => [number, number, number];
+export const init_panic_hook: () => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
